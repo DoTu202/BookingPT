cad89a03d0bca97a7f5ad5f6d92fb3cf91a65e29
MhTu202 <138098503+MhTu202@users.noreply.github.com>
2025-05-19 13:04:23 +0700

    Bottom Navigation

--- clients/src/navigators/TabNavigator.jsx
+++ clients/src/navigators/TabNavigator.jsx
@@ -1,14 +1,66 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import { HomeScreen } from '../screens';
+import {
+  HomeScreen,
+  SessionScreen,
+  ProfileScreen,
+  MessengerScreen,
+  ProgressScreen,
+} from '../screens';
+import appColors from '../constants/appColors';
+import {appInfo} from '../constants/appInfos';
+import { Home, Calendar, BarChart2, MessageSquare, User } from 'lucide-react-native';
 
 
 const TabNavigator = () => {
-   const Tab = createBottomTabNavigator();
+  const Tab = createBottomTabNavigator();
   return (
-    <Tab.Navigator screenOptions={{headerShown: false}}>
-      <Tab.Screen name="HomeScreen" component={HomeScreen} />
+    <Tab.Navigator 
+    screenOptions = {({route}) => ({
+      headerShown: false,
+      tabBarShowLabel: true,
+      tabBarStyle:{
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      tabBarIcon: ({focused, color, size}) => {
+        color = focused ? appColors.primary : appColors.gray;
+        let icon;
+        switch (route.name) {
+          case 'Home':
+            icon = <Home color={color} size={size} />;
+            break;
+          case 'Session':
+            icon = <Calendar color={color} size={size} />;
+            break;
+          case 'Progress':
+            icon = <BarChart2 color={color} size={size} />;
+            break;
+          case 'Messenger':
+            icon = <MessageSquare color={color} size={size} />;
+            break;
+          case 'Profile':
+            icon = <User color={color} size={size} />;
+            break;
+        }
+        return icon;
+      },
+      tabBarLabel: ({focused}) => {
+        const color = focused ? appColors.primary : appColors.gray;
+        return (
+          <Text style={{color: color, fontSize: 12}}>
+            {route.name}
+          </Text>
+        )
+      }
+    })} 
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Session" component={SessionScreen} />
+      <Tab.Screen name="Progress" component={ProgressScreen} />
+      <Tab.Screen name="Messenger" component={MessengerScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
   );
 };
