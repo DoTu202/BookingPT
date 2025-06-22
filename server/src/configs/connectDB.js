230e88d8d80691e032af5c9afdc528d010faf359
MhTu202 <138098503+MhTu202@users.noreply.github.com>
2025-06-23 02:10:44 +0700

    fix

--- server/src/configs/connectDB.js
+++ server/src/configs/connectDB.js
@@ -1,29 +1,21 @@
 require('dotenv').config();
 
-const { mongoose } = require('mongoose');
-
-
-
+const {mongoose} = require('mongoose');
 
 const dbURL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.zgfg3pd.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Local MongoDB (uncomment if you want to use local)
-// const dbURL = 'mongodb://localhost:27017/fitness-app';
-
 const connectDB = async () => {
-    try {
-        const connection = await mongoose.connect(dbURL, {
-            serverSelectionTimeoutMS: 30000, // 30 seconds timeout
-            connectTimeoutMS: 30000,
-            socketTimeoutMS: 30000,
-        }); 
-        console.log('Connected to MongoDB:', connection.connection.name);
-       
-    } catch (error) {
-        console.error('MongoDB connection error:', error);
-        process.exit(1);
-    }
+  try {
+    const connection = await mongoose.connect(dbURL, {
+      serverSelectionTimeoutMS: 30000,
+      connectTimeoutMS: 30000,
+      socketTimeoutMS: 30000,
+    });
+    console.log('Connected to MongoDB:', connection.connection.name);
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
 };
-
 
 module.exports = connectDB;
